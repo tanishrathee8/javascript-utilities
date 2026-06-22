e4658065224c5f46f2803f9a1f22dbaa901d07f1
Tanish Rathee <tanishrathee8@gmail.com>
2026-06-22 21:58:26 +0530

    Add second largest number finder utility

---
+++ findSecondLargest.js
@@ -0,0 +1,17 @@
+function findSecondLargest(arr) {
+  let largest = -Infinity;
+  let secondLargest = -Infinity;
+
+  for (let num of arr) {
+    if (num > largest) {
+      secondLargest = largest;
+      largest = num;
+    } else if (num > secondLargest && num !== largest) {
+      secondLargest = num;
+    }
+  }
+
+  return secondLargest;
+}
+
+console.log(findSecondLargest([10, 5, 20, 8, 15]));
