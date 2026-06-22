fa73b844c9658332b6d2077d1f8a570eff1b4401
Tanish Rathee <tanishrathee8@gmail.com>
2026-06-22 22:00:15 +0530

    Implement character frequency counter

---
+++ characterFrequency.js
@@ -0,0 +1,11 @@
+function characterFrequency(str) {
+  const freq = {};
+
+  for (let char of str) {
+    freq[char] = (freq[char] || 0) + 1;
+  }
+
+  return freq;
+}
+
+console.log(characterFrequency("javascript"));
