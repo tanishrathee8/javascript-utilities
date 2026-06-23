fcac397d4d389d4cfcf954bada0f5a8ee9313e70
Tanish Rathee <tanishrathee8@gmail.com>
2026-06-23 23:14:23 +0530

    Add array chunking utility

---
+++ chunkArray.js
@@ -0,0 +1,11 @@
+function chunkArray(arr, size) {
+  const chunks = [];
+
+  for (let i = 0; i < arr.length; i += size) {
+    chunks.push(arr.slice(i, i + size));
+  }
+
+  return chunks;
+}
+
+console.log(chunkArray([1,2,3,4,5,6,7,8], 3));
