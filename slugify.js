8007ac431edab4642e630e59af80733704c20f98
Tanish Rathee <tanishrathee8@gmail.com>
2026-07-04 23:22:27 +0530

    Add slug generator utility

---
+++ slugify.js
@@ -0,0 +1,10 @@
+function slugify(text) {
+  return text
+    .toLowerCase()
+    .trim()
+    .replace(/[^\w\s-]/g, "")
+    .replace(/\s+/g, "-")
+    .replace(/-+/g, "-");
+}
+
+console.log(slugify("My First JavaScript Project!"));
