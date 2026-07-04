784c9a185d57399d0fe33997563849a6bedb54f1
Tanish Rathee <tanishrathee8@gmail.com>
2026-07-04 23:23:16 +0530

    Add promise-based sleep utility

---
+++ sleep.js
@@ -0,0 +1,15 @@
+function sleep(ms) {
+  return new Promise(resolve => {
+    setTimeout(resolve, ms);
+  });
+}
+
+async function demo() {
+  console.log("Start");
+
+  await sleep(2000);
+
+  console.log("Finished after 2 seconds");
+}
+
+demo();
