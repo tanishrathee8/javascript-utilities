de93b5d8dc42dd46cf7c494ba7d8587d46952811
Tanish Rathee <tanishrathee8@gmail.com>
2026-07-09 12:04:41 +0530

    Add binary search utility

---
+++ arrays/binarySearch.js
@@ -0,0 +1,25 @@
+function binarySearch(arr, target) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+
+    if (arr[mid] === target) {
+      return mid;
+    }
+
+    if (arr[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return -1;
+}
+
+const numbers = [2, 4, 6, 8, 10, 12, 14];
+
+console.log(binarySearch(numbers, 10));
+console.log(binarySearch(numbers, 5));
