9000fda4726533c2e733ca3308afbe4353ae8a08
Tanish Rathee <tanishrathee8@gmail.com>
2026-07-10 12:04:53 +0530

    Add utility to remove duplicate objects by key

---
+++ arrays/removeDuplicateObjects.js
@@ -0,0 +1,22 @@
+
+
+function removeDuplicateObjects(arr, key) {
+  const seen = new Set();
+
+  return arr.filter(item => {
+    if (seen.has(item[key])) {
+      return false;
+    }
+
+    seen.add(item[key]);
+    return true;
+  });
+}
+
+const users = [
+  { id: 1, name: "Tanish" },
+  { id: 2, name: "Rahul" },
+  { id: 1, name: "Tanish" }
+];
+
+console.log(removeDuplicateObjects(users, "id"));
