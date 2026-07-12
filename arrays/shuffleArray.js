80876d0624d45a7e050981e5b00d9d0724caad8b
Tanish Rathee <tanishrathee8@gmail.com>
2026-07-12 14:45:19 +0530

    Add Fisher-Yates array shuffle utility

---
+++ arrays/shuffleArray.js
@@ -0,0 +1,13 @@
+function shuffleArray(arr) {
+  const result = [...arr];
+
+  for (let i = result.length - 1; i > 0; i--) {
+    const randomIndex = Math.floor(Math.random() * (i + 1));
+
+    [result[i], result[randomIndex]] = [result[randomIndex], result[i]];
+  }
+
+  return result;
+}
+
+console.log(shuffleArray([1, 2, 3, 4, 5]));
