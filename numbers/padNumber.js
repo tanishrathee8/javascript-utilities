723d4f695eed5703449afb609a4b0171e33e5ac2
Tanish Rathee <tanishrathee8@gmail.com>
2026-07-28 23:02:15 +0530

    Add input validation to pad number utility

--- numbers/padNumber.js
+++ numbers/padNumber.js
@@ -1,6 +1,10 @@
 function padNumber(number, length) {
+  if (!Number.isInteger(length) || length < 0) {
+    throw new Error("Length must be a non-negative integer.");
+  }
+
   return String(number).padStart(length, "0");
 }
 
 console.log(padNumber(7, 3));
-console.log(padNumber(25, 5));
+console.log(padNumber(125, 5));
