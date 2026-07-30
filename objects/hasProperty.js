07d2f7df16aec1751fefc86ada2f7ad1ce3fa1d1
Tanish Rathee <tanishrathee8@gmail.com>
2026-07-30 23:58:09 +0530

    Document object property checker

--- objects/hasProperty.js
+++ objects/hasProperty.js
@@ -1,7 +1,9 @@
 function hasProperty(obj, key) {
   return Object.prototype.hasOwnProperty.call(obj, key);
 }
-
+/**
+ * Checks whether an object contains the specified own property.
+ */
 const user = {
   name: "Tanish",
   age: 21,
