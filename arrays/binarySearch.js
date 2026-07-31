47ea23bcbdc0bc1a2fae01f4bf9e16c547c18395
Tanish Rathee <tanishrathee8@gmail.com>
2026-08-01 01:59:34 +0530

    Improve binary search documentation

--- arrays/binarySearch.js
+++ arrays/binarySearch.js
@@ -1,5 +1,9 @@
 /**
- * Performs binary search on a sorted array.
+ * Performs binary search on a sorted numeric array.
+ * Returns the index of the target or -1 if not found.
+ */
+/**
+
  * @param {number[]} arr
  * @param {number} target
  * @returns {number} Index of target or -1 if not found.
