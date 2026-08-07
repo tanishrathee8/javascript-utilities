1204bdd06bbce6b8f45697bdf8f800c4856c3707
Tanish Rathee <tanishrathee8@gmail.com>
2026-08-07 11:28:31 +0530

    Add utility to filter unique objects by key

---
+++ arrays/uniqueObjectsByKey.js
@@ -0,0 +1,20 @@
+function uniqueObjectsByKey(arr, key) {
+  const seen = new Set();
+
+  return arr.filter(item => {
+    if (seen.has(item[key])) {
+      return false;
+    }
+
+    seen.add(item[key]);
+    return true;
+  });
+}
+
+const users = [
+  { id: 1, name: "Tanish" },
+  { id: 2, name: "Rahul" },
+  { id: 1, name: "Tanish" }
+];
+
+console.log(uniqueObjectsByKey(users, "id"));
