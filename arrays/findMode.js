1bcfd6c5aa42d4adb0b5e81e594d9b0a64eb8fbf
Tanish Rathee <tanishrathee8@gmail.com>
2026-08-08 00:42:46 +0530

    Add mode finder for numeric arrays

---
+++ arrays/findMode.js
@@ -0,0 +1,18 @@
+function findMode(arr) {
+  const frequency = {};
+  let mode = arr[0];
+  let maxCount = 0;
+
+  for (const num of arr) {
+    frequency[num] = (frequency[num] || 0) + 1;
+
+    if (frequency[num] > maxCount) {
+      maxCount = frequency[num];
+      mode = num;
+    }
+  }
+
+  return mode;
+}
+
+console.log(findMode([1, 2, 2, 3, 4, 2, 5]));
