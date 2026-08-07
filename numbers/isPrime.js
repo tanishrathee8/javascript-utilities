d673325367eb74079e4a1be45f07d9f3f328625c
Tanish Rathee <tanishrathee8@gmail.com>
2026-08-08 00:43:57 +0530

    Improve prime number utility with integer validation

--- numbers/isPrime.js
+++ numbers/isPrime.js
@@ -1,5 +1,7 @@
 function isPrime(num) {
-  if (num < 2) return false;
+  if (!Number.isInteger(num) || num < 2) {
+    return false;
+  }
 
   for (let i = 2; i <= Math.sqrt(num); i++) {
     if (num % i === 0) {
