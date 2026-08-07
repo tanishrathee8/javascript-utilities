7bb54b000de8a7957874e602b62428663069b7c8
Tanish Rathee <tanishrathee8@gmail.com>
2026-08-08 00:47:33 +0530

    Improve GCD utility with input validation

--- numbers/gcd.js
+++ numbers/gcd.js
@@ -1,9 +1,17 @@
 function gcd(a, b) {
+  if (!Number.isInteger(a) || !Number.isInteger(b)) {
+    throw new TypeError("Inputs must be integers.");
+  }
+
+  a = Math.abs(a);
+  b = Math.abs(b);
+
   while (b !== 0) {
     [a, b] = [b, a % b];
   }
 
-  return Math.abs(a);
+  return a;
 }
 
 console.log(gcd(24, 18));
+console.log(gcd(-24, 18));
