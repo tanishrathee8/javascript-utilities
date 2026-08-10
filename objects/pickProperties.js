302ec6148a2465381c8e7d18a2c23347f9de4cf7
Tanish Rathee <tanishrathee8@gmail.com>
2026-08-10 23:23:42 +0530

    Add object property selection utility

---
+++ objects/pickProperties.js
@@ -0,0 +1,19 @@
+function pickProperties(obj, keys) {
+  const result = {};
+
+  for (const key of keys) {
+    if (key in obj) {
+      result[key] = obj[key];
+    }
+  }
+
+  return result;
+}
+
+const user = {
+  name: "Tanish",
+  age: 21,
+  city: "Delhi"
+};
+
+console.log(pickProperties(user, ["name", "age"]));
