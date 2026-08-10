f3e868907ae649405a767838f48f6edf46438839
Tanish Rathee <tanishrathee8@gmail.com>
2026-08-10 23:24:29 +0530

    Improve alphabetic validator with input checks

--- strings/isAlphabetic.js
+++ strings/isAlphabetic.js
@@ -1,6 +1,11 @@
 function isAlphabetic(str) {
+  if (typeof str !== "string" || str.length === 0) {
+    return false;
+  }
+
   return /^[A-Za-z]+$/.test(str);
 }
 
 console.log(isAlphabetic("JavaScript"));
 console.log(isAlphabetic("Java123"));
+console.log(isAlphabetic(""));
