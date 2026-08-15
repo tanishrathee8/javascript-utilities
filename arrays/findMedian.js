18bc7d9ab9633ef936070349e4ccb99f7233e759
Tanish Rathee <tanishrathee8@gmail.com>
2026-08-15 10:53:36 +0530

    Add median calculation utility

---
+++ arrays/findMedian.js
@@ -0,0 +1,17 @@
+function findMedian(arr) {
+  if (!Array.isArray(arr) || arr.length === 0) {
+    return null;
+  }
+
+  const sorted = [...arr].sort((a, b) => a - b);
+  const middle = Math.floor(sorted.length / 2);
+
+  if (sorted.length % 2 === 0) {
+    return (sorted[middle - 1] + sorted[middle]) / 2;
+  }
+
+  return sorted[middle];
+}
+
+console.log(findMedian([5, 2, 8, 1, 4]));
+console.log(findMedian([1, 2, 3, 4]));
