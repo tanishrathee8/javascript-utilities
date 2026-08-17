a19d222055e49e52cb2a1ea8e5a0711d6bfa16ef
Tanish Rathee <tanishrathee8@gmail.com>
2026-08-18 00:09:18 +0530

    Add object grouping utility

---
+++ objects/groupBy.js
@@ -0,0 +1,21 @@
+function groupBy(arr, key) {
+  return arr.reduce((groups, item) => {
+    const value = item[key];
+
+    if (!groups[value]) {
+      groups[value] = [];
+    }
+
+    groups[value].push(item);
+
+    return groups;
+  }, {});
+}
+
+const users = [
+  { name: "Tanish", role: "developer" },
+  { name: "Rahul", role: "designer" },
+  { name: "Aman", role: "developer" }
+];
+
+console.log(groupBy(users, "role"));
