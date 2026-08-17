d0a5729224630bd4e58e96d0bc4551408e9675b6
Tanish Rathee <tanishrathee8@gmail.com>
2026-08-17 23:50:09 +0530

    Improve numeric validator input handling

--- strings/isNumeric.js
+++ strings/isNumeric.js
@@ -1,10 +1,17 @@
 function isNumeric(str) {
-  if (typeof str !== "string" || str.trim() === "") {
+  if (typeof str !== "string") {
     return false;
   }
 
-  return /^\d+$/.test(str.trim());
+  const value = str.trim();
+
+  if (value === "") {
+    return false;
+  }
+
+  return /^\d+$/.test(value);
 }
 
 console.log(isNumeric("12345"));
 console.log(isNumeric("123abc"));
+console.log(isNumeric(" 456 "));
