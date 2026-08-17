faade11fab3ffc846e86ea49bf7eafccf25df18f
Tanish Rathee <tanishrathee8@gmail.com>
2026-08-18 00:08:14 +0530

    Add nested array flattening utility

--- arrays/flattenArray.js
+++ arrays/flattenArray.js
@@ -2,4 +2,8 @@ function flattenArray(arr) {
   return arr.flat();
 }
 
-console.log(flattenArray([[1,2],[3,4],[5,6]]));
+console.log(flattenArray([[1,2],[3,4],[5,6]]));function flattenArray(arr) {
+  return arr.flat(Infinity);
+}
+
+console.log(flattenArray([1, [2, [3, 4]], 5]));
