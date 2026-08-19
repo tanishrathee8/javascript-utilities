b69fff84b652e2500bb6e45c0ee70df184c00cdc
Tanish Rathee <tanishrathee8@gmail.com>
2026-08-19 23:23:35 +0530

    Add safe object property getter

---
+++ objects/getProperty.js
@@ -0,0 +1,17 @@
+function getProperty(obj, key, defaultValue = null) {
+  if (obj === null || typeof obj !== "object") {
+    throw new TypeError("Input must be an object.");
+  }
+
+  return Object.prototype.hasOwnProperty.call(obj, key)
+    ? obj[key]
+    : defaultValue;
+}
+
+const user = {
+  name: "Tanish",
+  age: 21
+};
+
+console.log(getProperty(user, "name"));
+console.log(getProperty(user, "city", "Unknown"));
