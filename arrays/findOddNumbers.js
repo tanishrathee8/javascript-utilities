481cd5abae033a65b97bfba860041d4c7a3d3561
Tanish Rathee <tanishrathee8@gmail.com>
2026-08-21 00:28:59 +0530

    Add odd number filtering utility

---
+++ arrays/findOddNumbers.js
@@ -0,0 +1,9 @@
+function findOddNumbers(arr) {
+  if (!Array.isArray(arr)) {
+    throw new TypeError("Input must be an array.");
+  }
+
+  return arr.filter(num => num % 2 !== 0);
+}
+
+console.log(findOddNumbers([1, 2, 3, 4, 5, 6, 7]));
