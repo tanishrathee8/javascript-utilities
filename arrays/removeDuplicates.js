baf3ef50bd4d2ede7227cdce8881b5bcd36242dd
Tanish Rathee <tanishrathee8@gmail.com>
2026-08-23 22:28:29 +0530

    Add edge cases for duplicate removal utility

--- arrays/removeDuplicates.js
+++ arrays/removeDuplicates.js
@@ -7,3 +7,6 @@ function removeDuplicates(arr) {
 }
 
 console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5]));
+console.log(removeDuplicates([]));
+console.log(removeDuplicates([1, 1, 1, 1]));
+console.log(removeDuplicates(["a", "b", "a", "c"]));
