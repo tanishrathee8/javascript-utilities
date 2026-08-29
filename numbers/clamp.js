d1f9c0dc2921ebed3ca3f7fceab74ef1054711e0
Tanish Rathee <tanishrathee8@gmail.com>
2026-08-29 23:38:34 +0530

    Added edge cases

--- numbers/clamp.js
+++ numbers/clamp.js
@@ -2,6 +2,5 @@ function clamp(value, min, max) {
   return Math.min(Math.max(value, min), max);
 }
 
-console.log(clamp(15, 1, 10));
-console.log(clamp(-5, 1, 10));
-console.log(clamp(7, 1, 10));
+console.log(clamp(150, 0, 100));
+console.log(clamp(-10, 0, 100));
