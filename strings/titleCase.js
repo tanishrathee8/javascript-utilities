ca01745ad44a1a0bce522259c2b5c69e01b1b263
Tanish Rathee <tanishrathee8@gmail.com>
2026-08-29 23:34:09 +0530

    Add title case string utility

--- strings/titleCase.js
+++ strings/titleCase.js
@@ -1,10 +1,9 @@
 function titleCase(str) {
   return str
+    .toLowerCase()
     .split(" ")
-    .map(word =>
-      word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
-    )
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
     .join(" ");
 }
 
-console.log(titleCase("welcome to javascript utilities"));
+console.log(titleCase("javascript utility collection"));
