2ab0c7b4a2acdb6d0a9b331dfc50e890d48269fb
Tanish Rathee <tanishrathee8@gmail.com>
2026-09-02 23:12:37 +0530

    Add browser text file download utility

---
+++ browser/downloadTextFile.js
@@ -0,0 +1,22 @@
+function downloadTextFile(filename, content) {
+  const blob = new Blob([content], {
+    type: "text/plain"
+  });
+
+  const url = URL.createObjectURL(blob);
+  const link = document.createElement("a");
+
+  link.href = url;
+  link.download = filename;
+
+  document.body.appendChild(link);
+  link.click();
+  link.remove();
+
+  URL.revokeObjectURL(url);
+}
+
+downloadTextFile(
+  "example.txt",
+  "Hello from JavaScript!"
+);
