d98ff148a5f4ee6965c58661ba302551041460a1
Tanish Rathee <tanishrathee8@gmail.com>
2026-09-08 12:33:27 +0530

    Add function currying utility

---
+++ functions/curry.js
@@ -0,0 +1,18 @@
+function curry(fn) {
+  return function curried(...args) {
+    if (args.length >= fn.length) {
+      return fn(...args);
+    }
+
+    return (...nextArgs) => curried(...args, ...nextArgs);
+  };
+}
+
+function add(a, b, c) {
+  return a + b + c;
+}
+
+const curriedAdd = curry(add);
+
+console.log(curriedAdd(1)(2)(3));
+console.log(curriedAdd(1, 2)(3));
