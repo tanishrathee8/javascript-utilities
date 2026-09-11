212a11d6e6b784624878018557163aca9bbf98c9
Tanish Rathee <tanishrathee8@gmail.com>
2026-09-11 20:51:46 +0530

    Add browser network status utility

---
+++ browser/isOnline.js
@@ -0,0 +1,18 @@
+function isOnline() {
+  return navigator.onLine;
+}
+
+function watchNetworkStatus(callback) {
+  const handleOnline = () => callback(true);
+  const handleOffline = () => callback(false);
+
+  window.addEventListener("online", handleOnline);
+  window.addEventListener("offline", handleOffline);
+
+  return () => {
+    window.removeEventListener("online", handleOnline);
+    window.removeEventListener("offline", handleOffline);
+  };
+}
+
+console.log(isOnline());
